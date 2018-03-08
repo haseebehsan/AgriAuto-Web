6e182ff84a64fc29e391832217d3ec065ed77e25
Haseeb Ehsan <haseeb_ehsan@yahoo.com>
2018-03-08 15:29:02 +0500

    page name bug

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
   app.get('/', (req, res) => res.render('pages/index'));
 
 
-  app.get('/api/login', (req, res) => res.render('pages/hello'));
+  app.get('/api/login', (req, res) => res.render('pages/test'));
 
 
 
