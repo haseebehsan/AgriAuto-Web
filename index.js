7cc6b904026908bd243f4a71107410178841717c
Haseeb Ehsan <haseeb_ehsan@yahoo.com>
2018-03-08 14:59:28 +0500

    login pages added, test for login page

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+  app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  app.get('/api/login', (req, res) => res.render('pages/login'));
+
+
+
+
+
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
