0676c791987357b94878fc49cb6263a164b77f5c
Haseeb Ehsan <haseeb_ehsan@yahoo.com>
2018-03-08 15:15:48 +0500

    test page added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
   app.get('/', (req, res) => res.render('pages/index'));
 
 
-  app.get('/api/login', (req, res) => res.render('pages/login'));
+  app.get('/api/login', (req, res) => res.render('pages/hello'));
 
 
 
