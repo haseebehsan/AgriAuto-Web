41215e3380e3c24455b46c367ff0eb487a6017d9
Haseeb Ehsan <haseeb_ehsan@yahoo.com>
2018-03-17 01:51:41 +0500

    getLatestSensorData
    printing test

--- index.js
+++ index.js
@@ -321,6 +321,77 @@ app.post('/api/getSensorData', function (req, res) {
 
 
 
+////////////////---------------------------------------------------------------
+// All API POST requests
+////////////////---------------------------------------------------------------  
+
+
+/////////////
+//inputs:
+//  farmid: 0000-9999 
+//  siteid
+//output
+//  sensor data in JSON
+/////////////
+app.post('/api/getLatestSensorData', function (req, res) {
+
+  console.log(req.body.farmid);
+  console.log(req.body.siteid);
+  console.log(req.body.startdate);
+  console.log(req.body.enddate);
+
+ 
+  var returnData = "{ ";
+  var count = 0;
+
+  firebase.database().ref('/farms/' + req.body.farmid + '/' + req.body.siteid + '/sensor/').orderByKey().limitToLast(1).then(function (snapshot) {
+    var childstring;
+    var highest = '';
+    console.log(JSON.stringify(snapshot));
+    // snapshot.forEach(function (childSnapshot) {
+    //   // console.log("key: "+childSnapshot.key);
+
+    //   ///checks which is latest date
+    //   if (childSnapshot.key >= highest){
+        
+    //   } 
+    //     // console.log("key: "+childSnapshot.key);
+    //     childstring = JSON.stringify(childSnapshot.val());
+    //     // console.log(childstring);
+    //     // childstring = childstring.substring(1, childstring.length -1);
+    //     childstring = "\"" + childSnapshot.key + "\":" + childstring;
+    //     if (count == 0) {
+
+    //       count = 1
+    //     }
+    //     else {
+    //       childstring = "," + childstring;
+    //     }
+
+    //     returnData += childstring;
+    //     console.log(returnData);
+
+      
+
+    });
+
+    // returnData += " }";
+
+    console.log(snapshot.val());
+    // res.json(JSON.parse(returnData));
+    res.json(JSON.parse(snapshot));
+    //var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    // ...
+  });
+
+
+  res.status(200);
+  // res.render('index');
+});
+
+
+
+
 
 
 /////////////
@@ -425,8 +496,7 @@ app.post('/api/setSensorData', function (req, res) {
 //inputs:
 //  farmid
 //  siteid
-//  irrigation status: 0,1 
-//  fieldid: 0000-9999
+//  irrigation status: 0,1
 //output
 //  status
 /////////////
