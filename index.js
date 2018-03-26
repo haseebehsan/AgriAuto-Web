9d0589f53460420e149f1ec0fc0144108aba8791
Hamza Rashid Bodla <hamzabodla@gmail.com>
2018-03-26 17:36:36 +0500

    Revert "Auto stash before revert of "missing checks added""
    This reverts commit 6863ebd9d443224e000fd028e797cb76aeb310ac.

--- index.js
+++ index.js
@@ -361,13 +361,10 @@ app.get('/irrigation', function (req, res) {
       console.log("mode output: " + JSON.stringify(snapshot.val()));
       if (snapshot.val() != null) {
 
-        // siteid = siteid[1];
-        console.log("farmid: " + farmid + " - siteod: " + siteid);
-        
-        firebase.database().ref('/farms/' + farmid + '/' + siteid + '/irrigation/mode/mode').once('value').then(function (snapshot) {
-          // snapshot.forEach(function(childSnapshot) {
-          //     console.log(JSON.stringify(childSnapshot.val()));
-          //   });
+        console.log("mode output: " + JSON.stringify(snapshot.val()));
+        if (snapshot.val() != null) {
+         
+          irrigationMode = snapshot.val()
 
           console.log("mode output: " + JSON.stringify(snapshot.val()));
           if (snapshot.val() != null) {
@@ -455,24 +452,7 @@ app.get('/settings', function (req, res) {
 
 
 
-      res.render('settings', {
-        smmin: minsm,
-        smmax: maxsm,
-        hummin: minhum,
-        hummax: maxhum,
-        tempmin: mintemp,
-        tempmax: maxtemp,
-        fid: farmid,
-        sid: siteid
-      });
-    }, function () {
-      res.send('none');
-    });
-  }
-  //console.log("irrigation mode in irrigation page  -- " + irrigationMode)
-
-
-
+  
 });
 
 app.get('/about', function (req, res) {
@@ -670,7 +650,7 @@ app.post('/api/setSensorData', function (req, res) {
   console.log(req.body.hum);
   console.log(req.body.sm);
 
-  // Set Sensor Data
+  // Set Sample Data
   firebase.database().ref('/farms/' + req.body.farmid + '/' + req.body.siteid + '/sensor/' + req.body.date + '/' + req.body.time).set({
     hum: req.body.hum,
     sm: req.body.sm,
@@ -1217,7 +1197,7 @@ app.post('/api/getIrrigationMode', function (req, res) {
 //  siteid
 //  mode
 //
-//Sets the irrigation mode
+//
 ///////////// 
 app.post('/api/setIrrigationMode', function (req, res) {
   // res.status(200).json({ status: 'working' });
